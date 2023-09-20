8ab66c9f9014491a4bded976168cf7f141966229
Hasibur Rahman <hasibur.rahman@sazim.io>
2023-09-20 17:54:08 +0600

    add sign in authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <ColorSchemeScript />
       </head>
       <body className={inter.className}>
--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,18 +1,17 @@
 "use client";
-
-import signUp from "@/firebase/auth/signup";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
+import signIn from "@/firebase/auth/signin";
+import { useRouter } from "next/navigation";
 
-export default function Signup() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
 
   const handleForm = async (event) => {
     event.preventDefault();
 
-    const { result, error } = await signUp(email, password);
+    const { result, error } = await signIn(email, password);
 
     if (error) {
       return console.log(error);
@@ -20,38 +19,39 @@ export default function Signup() {
 
     // else successful
     console.log(result);
-    return router.push("/");
+    return router.push("/user/2");
   };
   return (
     <div className="wrapper border-2 border-red-400 w-screen">
       <div className="flex justify-center items-center form-wrapper border-2 border-green-800 h-screen ">
+        {/* <h1 className="mt-60 mb-30">Sign up</h1> */}
         <form onSubmit={handleForm} className="form">
           <label htmlFor="email">
             <p>Email</p>
             <input
-              className="border-2 border-gray-400"
               onChange={(e) => setEmail(e.target.value)}
               required
               type="email"
               name="email"
               id="email"
               placeholder="example@mail.com"
+              className="border-2 border-gray-600"
             />
           </label>
           <label htmlFor="password">
             <p>Password</p>
             <input
-              className="border-2 border-gray-400"
               onChange={(e) => setPassword(e.target.value)}
               required
               type="password"
               name="password"
               id="password"
               placeholder="password"
+              className="border-2 border-gray-600"
             />
           </label>
-          <button type="submit" className="border-2 border-blue-600">
-            Sign up
+          <button type="submit" className="border-2 border-green-600">
+            Sign in
           </button>
         </form>
       </div>
